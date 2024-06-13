c8ee6654e5d9efad2961025f6f1b7ec879c89baa
donghoon_han <donghoon_han@tmaxsoft.co.kr>
2024-06-13 15:02:16 +0900

    스프링 입문 초기 부분

---
+++ 스프링 입문/hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>(); // 실무에선 동시성 문제로 concurrent로 해야함.
+    private static long sequence = 0L; //동시성 문제로 Atomic Long을 써야함
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // null이어도 클라이언트의 작업이 가능하다.
+    }
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+    public void clearStore() {
+        store.clear();
+    }
+}
