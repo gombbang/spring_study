31122e58c46d6924e92a85bc00f8b0400456e990
donghoon_han <donghoon_han@tmaxsoft.co.kr>
2024-06-26 11:28:30 +0900

    mvc

--- 스프링 입문/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ 스프링 입문/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 // 컨트롤러는 어쩔 수 없이 애노테이션으로 처리해야한다, 컨테이너가 관리하므로.
 @Controller
@@ -19,4 +25,23 @@ public class MemberController {
     // 여러 인스턴스 생성이 필요없이 공용의 인스턴스면 되므로, spring container에 넣는다.
 
 
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";    // home 화면으로 redirect
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
